69c889913f16838c62583796c4fd6290fa71c421
Furkan AŞIK <126895954+frknask@users.noreply.github.com>
2023-07-11 16:58:07 +0300

    Delete sahne geçişi.cs

--- KODLAR/sahne geçişi.cs
+++
@@ -1,9 +0,0 @@
-xpublic void OnTriggerEnter2D(Collider2D collision)
-    {
-        
-        if (collision.tag == "door")
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
-        }
-
-    }
